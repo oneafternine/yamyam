a72194f4206c5e970c28af2766fc0395cf16af41
Chris Smith <chris@substrakt.com>
2017-03-21 21:53:05 +0000

    ADD: newsletter row

--- wp-content/themes/yamyamcards/template-parts/content-quick-info.php
+++ wp-content/themes/yamyamcards/template-parts/content-quick-info.php
@@ -16,7 +16,7 @@
                 <div class="c-info_item__icon">
                     <img src="<?php echo get_template_directory_uri() ?>/assets/images/head-small--yellow.png" width="80" height="80">
                 </div>
-                <h5 class="c-info_item__title">50p postage &amp; packaging across the UK</h3>
+                <h5 class="c-info_item__title">Free UK postage on all orders over £10</h3>
             </div>
             <div class="c-info-row__item o-layout__item  u-1/1  u-1/3@tablet">
                 <div class="c-info_item__icon">
