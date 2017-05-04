f1585674f5e24dec5cd48b9824d726c6282ad8b6
Chris Smith <chris@substrakt.com>
2017-05-04 21:16:32 +0100

    UPDATE: remove ds store, add style.css to root

--- wp-content/themes/yamyamcards/template-parts/content-newsletter-signup.php
+++ wp-content/themes/yamyamcards/template-parts/content-newsletter-signup.php
@@ -25,18 +25,19 @@
                             <div id="mc_embed_signup_scroll">
 
                         <div class="mc-field-group">
-                            <!-- <label for="mce-EMAIL">Email Address: </label> -->
-                            <input type="email" value="" name="EMAIL" class="required email form__field" id="mce-EMAIL" placeholder="Email address">
+                            <label for="mce-EMAIL">Email Address: </label>
+                            <input type="email" value="" name="EMAIL" class="required email form__field" id="mce-EMAIL">
                         </div>
                             <div id="mce-responses" class="clear">
                                 <div class="response" id="mce-error-response" style="display:none"></div>
                                 <div class="response" id="mce-success-response" style="display:none"></div>
                             </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
                             <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_f16e6027cdc1a835756dc172b_6b9e624fbb" tabindex="-1" value=""></div>
-                            <input type="submit" value="Tell me about awesome cards!" name="subscribe" id="mc-embedded-subscribe" class="button form__btn">
+                            <div class="clear"><input type="submit" value="Tell me about awesome cards" name="subscribe" id="mc-embedded-subscribe" class="button form__btn"></div>
                             </div>
                         </form>
                         </div>
+
                         <!--End mc_embed_signup-->
                     </div>
                 </div>
