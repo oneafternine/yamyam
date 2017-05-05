2ca800bd0b8f374087b2aa9a99653836031a7156
Chris Smith <chris@substrakt.com>
2017-05-05 18:22:11 +0100

    UPDATE: 404 page content

--- wp-content/themes/yamyamcards/404.php
+++ wp-content/themes/yamyamcards/404.php
@@ -9,56 +9,25 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+    <main class="site-main" role="main">
+        <div class="o-container o-row">
+            <div class="o-max-800 o-block--centred">
 
-			<section class="error-404 not-found">
-				<header class="page-header">
-					<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'yamyamcards' ); ?></h1>
-				</header><!-- .page-header -->
+    			<section class="error-404 not-found">
+    				<header class="page-header">
+    					<h1 class="page-title"><?php esc_html_e( 'Yam lost bab!', 'yamyamcards' ); ?></h1>
+    				</header><!-- .page-header -->
 
-				<div class="page-content">
-					<p><?php esc_html_e( 'It looks like nothing was found at this location. Maybe try one of the links below or a search?', 'yamyamcards' ); ?></p>
+    				<div class="page-content">
 
-					<?php
-						get_search_form();
+                        <p class="intro">It seems the page you're looking for doesn't exist, or you could have mistyped a url.</p>
+                        <p>Head back to the <a href="/">Homepage</a> and try again.</p>
 
-						the_widget( 'WP_Widget_Recent_Posts' );
-
-						// Only show the widget if site has multiple categories.
-						if ( yamyamcards_categorized_blog() ) :
-					?>
-
-					<div class="widget widget_categories">
-						<h2 class="widget-title"><?php esc_html_e( 'Most Used Categories', 'yamyamcards' ); ?></h2>
-						<ul>
-						<?php
-							wp_list_categories( array(
-								'orderby'    => 'count',
-								'order'      => 'DESC',
-								'show_count' => 1,
-								'title_li'   => '',
-								'number'     => 10,
-							) );
-						?>
-						</ul>
-					</div><!-- .widget -->
-
-					<?php
-						endif;
-
-						/* translators: %1$s: smiley */
-						$archive_content = '<p>' . sprintf( esc_html__( 'Try looking in the monthly archives. %1$s', 'yamyamcards' ), convert_smilies( ':)' ) ) . '</p>';
-						the_widget( 'WP_Widget_Archives', 'dropdown=1', "after_title=</h2>$archive_content" );
-
-						the_widget( 'WP_Widget_Tag_Cloud' );
-					?>
-
-				</div><!-- .page-content -->
-			</section><!-- .error-404 -->
-
-		</main><!-- #main -->
-	</div><!-- #primary -->
+    				</div><!-- .page-content -->
+    			</section><!-- .error-404 -->
+            </div>
+        </div>
+	</main><!-- #main -->
 
 <?php
 get_footer();
