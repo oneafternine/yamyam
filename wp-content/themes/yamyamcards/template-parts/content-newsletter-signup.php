9ad4ef42f26f5ee627cf3ec134689dd7c11d4efc
Chris Smith <chris@substrakt.com>
2017-05-23 22:54:29 +0100

    UPDATE: messaging on newsletter sign up

--- wp-content/themes/yamyamcards/template-parts/content-newsletter-signup.php
+++ wp-content/themes/yamyamcards/template-parts/content-newsletter-signup.php
@@ -16,7 +16,7 @@
                     <img src="<?php echo get_template_directory_uri() ?>/assets/images/header_logo2x.png" alt="yamyam footer logo">
                 </div>
                 <div class="o-layout__item  u-1/1  u-3/4@tablet">
-                    <h3 class="c-newsletter__title">yamyamcard offers straight to your inbox!</h3>
+                    <h3 class="c-newsletter__title">10% off your first order, plus loads more offers straight to your inbox!</h3>
                     <p class="c-newsletter__description">Emails will only ever be about our awesome cards, and you can unsubscribe at any time</p>
                     <div class="newsletter__form">
                         <!-- Begin MailChimp Signup Form -->
@@ -33,7 +33,7 @@
                                 <div class="response" id="mce-success-response" style="display:none"></div>
                             </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
                             <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_f16e6027cdc1a835756dc172b_6b9e624fbb" tabindex="-1" value=""></div>
-                            <div class="clear"><input type="submit" value="Tell me about awesome cards" name="subscribe" id="mc-embedded-subscribe" class="button form__btn"></div>
+                            <div class="clear"><input type="submit" value="Send me offers" name="subscribe" id="mc-embedded-subscribe" class="button form__btn"></div>
                             </div>
                         </form>
                         </div>
