ab12884cb2be66c260b8651bfafa48bc79908487
Chris Smith <chris@substrakthealth.com>
2018-09-26 21:14:23 +0100

    UPDATE: change quick info text

--- wp-content/themes/yamyamcards/template-parts/content-quick-info.php
+++ wp-content/themes/yamyamcards/template-parts/content-quick-info.php
@@ -22,7 +22,7 @@
                 <div class="c-info_item__icon">
                     <img src="<?php echo get_template_directory_uri() ?>/assets/images/head-small--pink.png" width="80" height="80">
                 </div>
-                <h5 class="c-info_item__title">Get 12 cards for the price of 10</h3>
+                <h5 class="c-info_item__title">4 card multipack discount</h3>
             </div>
             <div class="c-info-row__item o-layout__item  u-1/1  u-1/3@tablet">
                 <div class="c-info_item__icon">
